af4e4bfe6f480ad415a673d237ed034b1b168738
jafarbekh <zokirovjafar2004@gmail.com>
2023-01-17 09:09:30 +0500

    takrorlaash

---
+++ day1_2.py
@@ -0,0 +1,12 @@
+dostlar = []
+print('5 ta eng yaqin do\'stingizni kiriting')
+
+for n in range(5):
+
+    dostlar.append(input(f"{n+1}-do\'stingizni kiriting "))
+
+print(dostlar)
+
+for dost in dostlar :
+
+    print(f"bular {dost} mening eng yaqin do'stlarim")
