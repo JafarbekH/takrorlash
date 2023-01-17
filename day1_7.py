ff8864032a6b2682e26925bad802ca155c793571
jafarbekh <zokirovjafar2004@gmail.com>
2023-01-17 21:12:34 +0500

    takrorlash

---
+++ day1_7.py
@@ -0,0 +1,8 @@
+print('kiritilgan sonning kvadratini chiqaruvchi dastur')
+
+sonlar = list(range(1,13))
+
+for son in sonlar :
+    if son ==5 :
+        continue
+    print(f"{son}ning kvadrati {son**2} ga teng") 
